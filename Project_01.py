7c4635968132066972777769154c539d268854d1
Kiminyoung3 <pie235@naver.com>
2024-08-21 16:34:41 +0900

    수정

--- Project_01.py
+++ Project_01.py
@@ -8,63 +8,101 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import seaborn as sns
 
 # 1. CSV 파일 읽기
-df = pd.read_csv('./data/6.Heatstroke_patient_prediction_test_data.csv')
+df = pd.read_csv('./data/6.Heatstroke_patient_prediction_train_data.csv')
 
 # 2. 새로운 헤더 설정
 header = ['1날짜 및 시간', '2이송 인원', '3최고기온', '4평균기온', '5최저기온', '6일조시간', '7평균풍속(m/s)', '8평균운량', '9평균습도(%)',
           '10강수량합계(mm)', '11최소상대습도(%)', '12합계 전천 일사량(MJ/m^2)', '13평균 증기압(hPa)', '14평균 현지 기압(hPa)',
           '15평균 해면 기압(hPa)', '16최대풍속(m/s)', '17최대 순간 풍속(m/s)', '18최고-최저 기온차', '19체감온도', '20불쾌지수', '21월', '22요일', '23주말 및 공휴일',
-          '24낮_맑음 비율', '25낮_흐림 비율', '26낮_비 비율', '27밤_맑음 비율', '28밤_흐림 비율', '29밤_비 비율', '30밤_번개 있음',
-          '31전일 최고 기온 차이', '32전일 평균 기온 차이', '33전일 최저 기온 차이', '34최고 기온 이동 평균(5일간)', '35평균 기온 이동 평균(5일간)',
-          '36체감 온도 이동 평균(5일간)', '37불쾌지수 이동 평균(5일간)', '38전일의 이송 인원수', '39이송 인원수 이동 평균(5일간)', '40연도']
+          '24낮_맑음 비율', '25낮_흐림 비율', '26낮_비 비율', '27낮_번개 비율', '28밤_맑음 비율', '29밤_흐림 비율', '30밤_비 비율', '31밤_번개 있음',
+          '32전일 최고 기온 차이', '33전일 평균 기온 차이', '34전일 최저 기온 차이', '35최고 기온 이동 평균(5일간)', '36평균 기온 이동 평균(5일간)',
+          '37체감 온도 이동 평균(5일간)', '38불쾌지수 이동 평균(5일간)', '39전일의 이송 인원수', '40이송 인원수 이동 평균(5일간)', '41Year']
 # df.columns = header_english
-# header_english = ['1Date and Time', '2Transported Patients', '3Highest Temperature', '4Average Temperature', '5Lowest Temperature',
-#                   '6Sunshine Duration', '7Average Wind Speed (m/s)', '8Average Cloudiness', '9Average Humidity (%)',
-#                   '10Total Precipitation (mm)', '11Minimum Relative Humidity (%)', '12Total Solar Radiation (MJ/m^2)',
-#                   '13Average Vapor Pressure (hPa)', '14Average Local Pressure (hPa)', '15Average Sea Level Pressure (hPa)',
-#                   '16Maximum Wind Speed (m/s)', '16Maximum 순간 Wind Speed (m/s)', '18Temperature Difference (High-Low)', '19Apparent Temperature', '20Discomfort Index',
-#                   '21Month', '22Day of the Week', '23Weekend and Holidays', '24Day_Clear Percentage', '25Day_Cloudy Percentage',
-#                   '26Day_Rain Percentage', '27Night_Clear Percentage', '28Night_Cloudy Percentage', '29Night_Rain Percentage',
-#                   '30Night_Lightning Presence', '31Difference in High Temp from Previous Day', '32Difference in Avg Temp from Previous Day',
-#                   '33Difference in Low Temp from Previous Day', '345-Day Moving Average of High Temp', '355-Day Moving Average of Avg Temp',
-#                   '365-Day Moving Average of Apparent Temp', '375-Day Moving Average of Discomfort Index', '38Transported Patients on Previous Day',
-#                   '395-Day Moving Average of Transported Patients', '40Year']
-
+# header_english = [
+#     '1.Date', '2.Total Transported Patients', '3.Highest Temperature (℃)', '4.Average Temperature (℃)', '5.Lowest Temperature (℃)',
+#     '6.Sunshine Duration (hours)', '7.Average Wind Speed (m/s)', '8.Average Cloudiness (10-minute ratio)', '9.Average Humidity (%)',
+#     '10.Total Precipitation (mm)', '11.Minimum Relative Humidity (%)', '12.Total Solar Radiation (MJ/m²)', '13.Average Vapor Pressure (hPa)',
+#     '14.Average Local Pressure (hPa)', '15.Average Sea Level Pressure (hPa)', '16.Maximum Wind Speed (m/s)', '17.Maximum Gust Wind Speed (m/s)',
+#     '18.Temperature Difference (High-Low)', '19.Apparent Temperature (℃)', '20.Discomfort Index', '21.Month', '22.Day of the Week',
+#     '23.Weekend and Holidays', '24.Day_Clear Percentage', '25.Day_Cloudy Percentage', '26.Day_Rain Percentage', '27.Day_Lightning Presence',
+#     '28Night_Clear Percentage', 'Night_Cloudy Percentage', 'Night_Rain Percentage', 'Night_Lightning Presence',
+#     'Difference in High Temp from Previous Day', 'Difference in Average Temp from Previous Day', 'Difference in Low Temp from Previous Day',
+#     '5-Day Moving Average of High Temp', '5-Day Moving Average of Average Temp', '5-Day Moving Average of Apparent Temp',
+#     '5-Day Moving Average of Discomfort Index', 'Transported Patients on Previous Day', '5-Day Moving Average of Transported Patients', 'Year'
+# ]
 df.columns = header
 
 # 3. 변경된 CSV 파일 저장
-df.to_csv('./data/6.Heatstroke_test_new_header.csv', index=False)
+df.to_csv('./data/6.Heatstroke_train_new_header.csv', index=False)
 
 # 저장된 CSV 파일 다시 읽기
-data = pd.read_csv('./data/6.Heatstroke_test_new_header.csv')
+data = pd.read_csv('./data/6.Heatstroke_train_new_header.csv')
 
 
-#__________________________18.체감온도_________________________________________________________
+# '이송 인원' 열을 기준으로 정렬
+sorted_df_by_transport = df.sort_values(by='1날짜 및 시간')
 
-# X와 Y 설정
-X5 = data['18체감온도']
-Y5 = data['2이송 인원']
+# 정렬된 데이터 출력 (열의 간격이 밀리지 않도록)
+with pd.option_context('display.max_columns', None):  # 모든 열을 출력
+    print("Sorted by '1날짜 및 시간':")
+    print(sorted_df_by_transport.head())
 
-# 데이터 분포 시각화 (전체 데이터)
-plt.figure(figsize=(10, 6))
-plt.scatter(X5, Y5, color='LightPink', label='Actual Data Points', marker='*', s=30, alpha=0.5)
-plt.title("Average Humidity(%) vs Number of Heatstroke Patients")
-plt.xlabel("Average Humidity(%)")
-plt.ylabel("Number of Heatstroke Patients")
-plt.legend()
-plt.show()
-
-#__________________________17.최고-최저 기온차_________________________________________________________
-
+#__________________________21.월_________________________________________________________
+#
 # # X와 Y 설정
-# X5 = data['17최고-최저 기온차']
+# X5 = data['21월']
 # Y5 = data['2이송 인원']
 #
 # # 데이터 분포 시각화 (전체 데이터)
 # plt.figure(figsize=(10, 6))
 # plt.scatter(X5, Y5, color='LightPink', label='Actual Data Points', marker='*', s=30, alpha=0.5)
-# plt.title("Average Humidity(%) vs Number of Heatstroke Patients")
-# plt.xlabel("Average Humidity(%)")
+# plt.title("Discomfort Index vs Number of Heatstroke Patients")
+# plt.xlabel("Discomfort Index")
+# plt.ylabel("Number of Heatstroke Patients")
+# plt.legend()
+# plt.show()
+
+#__________________________20.불쾌지수_________________________________________________________
+#
+# # X와 Y 설정
+# X5 = data['20불쾌지수']
+# Y5 = data['2이송 인원']
+#
+# # 데이터 분포 시각화 (전체 데이터)
+# plt.figure(figsize=(10, 6))
+# plt.scatter(X5, Y5, color='LightPink', label='Actual Data Points', marker='*', s=30, alpha=0.5)
+# plt.title("Discomfort Index vs Number of Heatstroke Patients")
+# plt.xlabel("Discomfort Index")
+# plt.ylabel("Number of Heatstroke Patients")
+# plt.legend()
+# plt.show()
+
+#__________________________19.체감온도_________________________________________________________
+#
+# # X와 Y 설정
+# X5 = data['19체감온도']
+# Y5 = data['2이송 인원']
+#
+# # 데이터 분포 시각화 (전체 데이터)
+# plt.figure(figsize=(10, 6))
+# plt.scatter(X5, Y5, color='LightPink', label='Actual Data Points', marker='*', s=30, alpha=0.5)
+# plt.title("Apparent Temperature vs Number of Heatstroke Patients")
+# plt.xlabel("Apparent Temperature")
+# plt.ylabel("Number of Heatstroke Patients")
+# plt.legend()
+# plt.show()
+
+#__________________________18.최고-최저 기온차_________________________________________________________
+
+# # X와 Y 설정
+# X5 = data['18최고-최저 기온차']
+# Y5 = data['2이송 인원']
+#
+# # 데이터 분포 시각화 (전체 데이터)
+# plt.figure(figsize=(10, 6))
+# plt.scatter(X5, Y5, color='LightPink', label='Actual Data Points', marker='*', s=30, alpha=0.5)
+# plt.title("Temperature Difference (High-Low) vs Number of Heatstroke Patients")
+# plt.xlabel("Temperature Difference (High-Low)")
 # plt.ylabel("Number of Heatstroke Patients")
 # plt.legend()
 # plt.show()
@@ -79,8 +117,8 @@ plt.show()
 # # 데이터 분포 시각화 (전체 데이터)
 # plt.figure(figsize=(10, 6))
 # plt.scatter(X5, Y5, color='LightPink', label='Actual Data Points', marker='*', s=30, alpha=0.5)
-# plt.title("Average Humidity(%) vs Number of Heatstroke Patients")
-# plt.xlabel("Average Humidity(%)")
+# plt.title("Total Solar Radiation (MJ/m^2) vs Number of Heatstroke Patients")
+# plt.xlabel("Total Solar Radiation (MJ/m^2)")
 # plt.ylabel("Number of Heatstroke Patients")
 # plt.legend()
 # plt.show()
